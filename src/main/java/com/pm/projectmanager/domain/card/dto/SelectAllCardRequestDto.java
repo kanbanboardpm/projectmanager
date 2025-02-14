74e026ae5535f45f0fa720e69c1643655411184b
junbin <ghdwnsqls0@naver.com>
2025-02-14 14:49:01 +0900

    fix : card api 엔드포인트 수정

--- src/main/java/com/pm/projectmanager/domain/card/dto/SelectAllCardRequestDto.java
+++ src/main/java/com/pm/projectmanager/domain/card/dto/SelectAllCardRequestDto.java
@@ -1,8 +1,10 @@
 package com.pm.projectmanager.domain.card.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SelectAllCardRequestDto {
     private Long projectId;
 }
